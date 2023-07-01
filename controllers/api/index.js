24b555856130fde524ff55c3038667ef62be4b29
silentdiamond <132628168+sefenydiaz@users.noreply.github.com>
2023-07-01 18:44:24 -0400

    added login js

--- controllers/api/index.js
+++ controllers/api/index.js
@@ -1,9 +1,11 @@
 const router = require('express').Router();
+const dashboardRoutes = require('./dashboardRoutes')
 
 // NEED TO REQUIRE MODELS
 
 //NEED ROUTER.USE FOR MODELS
 
+router.use('/dashboard', dashboardRoutes)
 
 
 module.exports = router;
