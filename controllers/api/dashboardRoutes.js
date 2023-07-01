fad325326da4e1aa5cf9875535903175932b70c7
silentdiamond <132628168+sefenydiaz@users.noreply.github.com>
2023-07-01 19:13:05 -0400

    added session

--- controllers/api/dashboardRoutes.js
+++ controllers/api/dashboardRoutes.js
@@ -1,1 +1,31 @@
 // NEED TO GET USERS BLOGPOSTS
+const router = require('express').Router();
+const { BlogPost, User, Comment } = require('../models');
+// const withAuth = require('../utils/auth');
+
+//CRUD
+//GET all user blogposts
+router.get('/', async (req, res) => {
+    try {
+        if (!req.session.logged_in) {
+            return res.redirect('/login');
+        }
+
+        const userId = req.session.user_id;
+
+        const blogpostData = await BlogPost.findAll({
+            where: { user_id: userId },
+            include: [ Comment ]
+        });
+
+        const blogposts = blogpostData.map((blogpost) => blogpost.get({ plain: true}));
+
+        res.render('dashboard', {
+            blogposts,
+        });
+    } catch (err) {
+        res.status(500).json(err);
+    }
+});
+
+  module.exports = router;
--- controllers/api/index.js
+++ controllers/api/index.js
@@ -1,9 +1,6 @@
 const router = require('express').Router();
 const dashboardRoutes = require('./dashboardRoutes')
 
-// NEED TO REQUIRE MODELS
-
-//NEED ROUTER.USE FOR MODELS
 
 router.use('/dashboard', dashboardRoutes)
 
