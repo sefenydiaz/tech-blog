e6f6386a9d04981deca35b024a9e8cf0c324c09d
silentdiamond <132628168+sefenydiaz@users.noreply.github.com>
2023-07-11 11:38:25 -0400

    updated index

--- controllers/api/index.js
+++ controllers/api/index.js
@@ -1,8 +1,8 @@
 const router = require('express').Router();
 const dashboardRoutes = require('./dashboardRoutes')
-
+const userRoutes = require("./userRoutes")
 
 router.use('/dashboard', dashboardRoutes)
-
+router.use("/users", userRoutes)
 
 module.exports = router;
